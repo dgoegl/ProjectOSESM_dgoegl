eb1c696d40ee6472839174b7ab511b824e0bdc4a
dgoegl <david.goegl@bluewin.ch>
2025-03-18 15:34:49 +0100

    Update License in utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,12 @@
 converter.py - A simple temperature conversion tool.
 
 Author: David Goegl
-License: MIT
+License: Apache License 2.0
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
 """
 
 def celsius_to_fahrenheit(celsius):
